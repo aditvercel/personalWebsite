c92fe5a03444f4a48b9e5d471f91612e1684794d
Aditya Marzuk <116705521+adityamms@users.noreply.github.com>
2024-10-06 13:49:09 +0700

    updated profile input and all cms route

--- app/cms/page.js
+++ app/cms/page.js
@@ -1,11 +1,237 @@
-import React from "react";
+"use client";
+import React, { useEffect, useState } from "react";
+import IStoolbar from "../components/utils/IStoolbar";
+import api from "@/utils/axiosInstance";
+import photosaya from "@/public/images/photo_saya.png";
+import cvImage from "@/public/images/cvImage.png";
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  Button,
+  FormControl,
+  FormLabel,
+  Input,
+  Textarea,
+  useDisclosure,
+} from "@chakra-ui/react";
 
-export default function page() {
+export default function Page() {
+  const [homePageDatas, setHomePageDatas] = useState({
+    latestProject: {
+      items: [],
+      totalPages: 0,
+      currentPage: 0,
+    },
+    skeletons: {
+      latestProject: false,
+    },
+  });
+
+  const [latestProjectQuery, setLatestProjectQuery] = useState({
+    category: 0,
+    page: 1,
+    totalPages: 0,
+  });
+
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const [formData, setFormData] = useState({
+    name: "Adityamms",
+    description:
+      "a full-stack developer skilled in both front-end and back-end technologies...",
+    file: null,
+  });
+
+  const handleChange = (e) => {
+    const { name, value, files } = e.target;
+    setFormData({
+      ...formData,
+      [name]: files ? files[0] : value, // Handle file input separately
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(formData);
+    onClose(); // Close the modal after submitting
+  };
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setHomePageDatas((item) => ({
+        ...item,
+        skeletons: {
+          ...item.skeletons,
+          latestProject: false,
+        },
+      }));
+
+      try {
+        const res = await api.get(
+          `${process.env.NEXT_PUBLIC_NEXTAUTH_URL}/api/latestProject?category=${latestProjectQuery.category}&page=1`
+        );
+
+        if (res.data.statusCode === 200) {
+          setHomePageDatas((item) => ({
+            ...item,
+            latestProject: {
+              items: [...res.data.result.items],
+              totalPages: res.data.result.totalPages,
+              currentPage: res.data.result.currentPage,
+            },
+            skeletons: {
+              ...item.skeletons,
+              latestProject: true,
+            },
+          }));
+        }
+      } catch (err) {
+        setHomePageDatas((item) => ({
+          ...item,
+          skeletons: {
+            ...item.skeletons,
+            latestProject: false,
+          },
+        }));
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <div className="relative">
-      <div className="bg-gray-100 py-10 px-20 relative">
-        CMS Dashboard Opened soon
+      <IStoolbar title="Dashboard" />
+      <div className="flex gap-5">
+        {/*Main*/}
+        <div className="border bg-gray-300 w-full rounded-lg p-5">
+          <div className="flex gap-5 justify-between">
+            <div className="border bg-white h-[120px] w-full rounded-lg p-2 flex gap-2">
+              <div
+                className="h-full w-[120px] rounded-lg bg-gray-300"
+                style={{
+                  backgroundImage: `url(${photosaya.src || ""})`,
+                  backgroundSize: "cover",
+                  backgroundPosition: "center",
+                }}
+              ></div>
+              <div className="grid ml-2 w-[90%] h-[90%] justify-between">
+                <div className="mb-2 flex justify-between">
+                  <div className=" font-medium text-lg">Adityamms</div>
+                  <div
+                    className="cursor-pointer text-blue-500"
+                    onClick={onOpen}
+                  >
+                    edit
+                  </div>
+                </div>
+                <p className="w-[90%] h-[90%] overflow-y-scroll">
+                  a full-stack developer skilled in both front-end and back-end
+                  technologies. I design user interfaces, build server-side
+                  logic, and manage databases to create complete, efficient web
+                  applications. If you need someone who can handle all aspects
+                  of development, I`m here to help!
+                </p>
+              </div>
+            </div>
+            <div className="border bg-white h-[120px] w-[400px] rounded-lg relative p-2">
+              <div className="  text-end">edit</div>
+              <div className="absolute top-0 bottom-0 right-0 left-0 m-auto w-20 h-20">
+                <div
+                  className="h-full w-full rounded-lg cursor-pointer"
+                  style={{
+                    backgroundImage: `url(${cvImage.src || ""})`,
+                    backgroundSize: "cover",
+                    backgroundPosition: "center",
+                  }}
+                ></div>
+              </div>
+            </div>
+          </div>
+
+          {/* payment History Coming soon */}
+          <div>
+            <div className="mb-3 font-semibold text-lg mt-10">
+              payment History Coming soon
+            </div>
+            <div className="bg-white h-[350px]"></div>
+          </div>
+        </div>
+
+        {/*Project*/}
+        <div className="border bg-white w-[600px] h-[50vw] rounded-lg  p-5">
+          <div className="mb-3 font-semibold text-lg">Project</div>
+          <div className="w-full h-[95%] overflow-y-scroll grid gap-3 justify-center">
+            {homePageDatas.latestProject?.items?.map((item, index) => (
+              <div
+                key={index}
+                className="bg-black w-[200px] h-[140px] rounded-lg"
+                style={{
+                  backgroundImage: `url(${item.image || ""})`,
+                  backgroundSize: "cover",
+                  backgroundPosition: "center",
+                }}
+              ></div>
+            ))}
+          </div>
+        </div>
       </div>
+
+      {/* Modal for editing profile */}
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Edit Profile</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <form onSubmit={handleSubmit}>
+              <FormControl mb={4} isRequired>
+                <FormLabel>Name</FormLabel>
+                <Input
+                  type="text"
+                  name="name"
+                  value={formData.name}
+                  onChange={handleChange}
+                  placeholder="Enter your name"
+                />
+              </FormControl>
+
+              <FormControl mb={4} isRequired>
+                <FormLabel>Description</FormLabel>
+                <Textarea
+                  name="description"
+                  value={formData.description}
+                  onChange={handleChange}
+                  placeholder="Enter a description"
+                />
+              </FormControl>
+
+              <FormControl mb={4}>
+                <FormLabel>Upload PDF</FormLabel>
+                <Input
+                  type="file"
+                  name="file"
+                  accept=".pdf"
+                  onChange={handleChange}
+                />
+              </FormControl>
+
+              <ModalFooter>
+                <Button colorScheme="blue" mr={3} type="submit">
+                  Save
+                </Button>
+                <Button variant="ghost" onClick={onClose}>
+                  Cancel
+                </Button>
+              </ModalFooter>
+            </form>
+          </ModalBody>
+        </ModalContent>
+      </Modal>
     </div>
   );
 }
---
+++ app/cms/profile/page.js
@@ -0,0 +1,305 @@
+"use client";
+import {
+  TableContainer,
+  Table,
+  Thead,
+  Tr,
+  Th,
+  Tbody,
+  Td,
+  Button,
+  HStack,
+  IconButton,
+  useDisclosure,
+  AlertDialog,
+  AlertDialogOverlay,
+  AlertDialogContent,
+  AlertDialogHeader,
+  AlertDialogBody,
+  AlertDialogFooter,
+  Link,
+  Input,
+  InputGroup,
+  InputLeftElement,
+  useToast, // Import Toast for notifications
+} from "@chakra-ui/react";
+import {
+  DeleteIcon,
+  EditIcon,
+  InfoOutlineIcon,
+  SearchIcon,
+} from "@chakra-ui/icons";
+import { useRef, useEffect, useState } from "react";
+import api from "@/utils/axiosInstance";
+import IStoolbar from "@/app/components/utils/IStoolbar";
+import {
+  convertToIndonesianDate,
+  convertToIndonesianDateMonthAndYear,
+} from "@/utils/formmattedValue";
+import { Pagination } from "@mui/material";
+import Image from "next/image";
+
+export default function Page() {
+  const { isOpen, onOpen, onClose } = useDisclosure(); // Chakra Disclosure for AlertDialog
+  const [homePageDatas, setHomePageDatas] = useState({
+    journeyDatas: [],
+    deletedItemId: "",
+    totalPage: 1,
+  });
+  const [loadingDelete, setLoadingDelete] = useState(false); // Loading state for delete action
+  const [searchQuery, setSearchQuery] = useState(""); // State for search query
+  const [currentPage, setCurrentPage] = useState(1); // State for current page
+  const toast = useToast(); // Toast for notifications
+
+  const cancelRef = useRef(); // Ref for AlertDialog cancellation
+  const searchTimeoutRef = useRef(null); // Ref to store the timeout ID
+
+  const changeDeletedId = (id) => {
+    setHomePageDatas((item) => {
+      return { ...item, deletedItemId: id };
+    });
+  };
+
+  const fetchData = async (page = 1, query = "") => {
+    try {
+      const res = await api.get(
+        `${process.env.NEXT_PUBLIC_NEXTAUTH_URL}/api/journey/all-list?page=${page}&search=${query}`
+      );
+
+      if (res.data.statusCode === 200) {
+        setHomePageDatas((item) => ({
+          ...item,
+          totalPage: res.data.result.totalPages,
+          journeyDatas: res.data.result.items.map((itemChild) => {
+            return {
+              ...itemChild,
+              createdAt: convertToIndonesianDate(itemChild.createdAt),
+              updatedAt: convertToIndonesianDate(itemChild.updatedAt),
+              year: convertToIndonesianDateMonthAndYear(itemChild.year),
+            };
+          }),
+        }));
+      }
+    } catch (error) {
+      toast({
+        title: "Error",
+        description: "Failed to fetch data.",
+        status: "error",
+        duration: 3000,
+        isClosable: true,
+      });
+      console.error("Error fetching data:", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchData(currentPage, searchQuery); // Fetch data on mount and when search or page changes
+  }, []);
+
+  const handleDelete = async () => {
+    setLoadingDelete(true); // Set loading to true during deletion process
+    try {
+      const res = await api.delete(
+        `${process.env.NEXT_PUBLIC_NEXTAUTH_URL}/api/journey/delete`,
+        {
+          data: { id: homePageDatas.deletedItemId },
+        }
+      );
+
+      if (res.data.statusCode === 200) {
+        setHomePageDatas((prev) => ({
+          ...prev,
+          journeyDatas: prev.journeyDatas.filter(
+            (item) => item._id !== homePageDatas.deletedItemId
+          ),
+          deletedItemId: "", // Clear the deletedItemId
+        }));
+        toast({
+          title: "Success",
+          description: "Item deleted successfully.",
+          status: "success",
+          duration: 3000,
+          isClosable: true,
+        });
+      }
+    } catch (error) {
+      toast({
+        title: "Error",
+        description: "Failed to delete the item.",
+        status: "error",
+        duration: 3000,
+        isClosable: true,
+      });
+      console.error("Error deleting item:", error);
+    } finally {
+      setLoadingDelete(false); // Stop loading state after deletion or failure
+      onClose(); // Close the dialog after the delete operation
+    }
+  };
+
+  const handleSearchChange = (event) => {
+    const value = event.target.value;
+    setSearchQuery(value); // Update search term
+    setCurrentPage(1); // Reset to page 1 when searching
+
+    // Clear the previous timeout to avoid multiple API calls
+    if (searchTimeoutRef.current) {
+      clearTimeout(searchTimeoutRef.current);
+    }
+
+    // Set a new timeout to wait for 1 second before triggering the API call
+    searchTimeoutRef.current = setTimeout(() => {
+      // Trigger the fetchData function with the current page and query
+      fetchData(currentPage, value);
+    }, 800); // 1 second delay
+  };
+
+  // Handler for pagination change
+  const handlePageChange = (event, value) => {
+    setCurrentPage(value); // Update page
+    fetchData(value, searchQuery); // Fetch data with the updated page and current search query
+  };
+
+  return (
+    <div>
+      <IStoolbar title="Profile Manager" add="/cms/journey/create" />
+      <div className="rounded-lg bg-white p-5">
+        <div className="my-5">
+          <InputGroup>
+            <InputLeftElement pointerEvents="none">
+              <SearchIcon color="gray.300" />
+            </InputLeftElement>
+            <Input
+              variant="filled"
+              placeholder="Search..."
+              className="border border-black"
+              value={searchQuery} // Controlled input for search
+              onChange={handleSearchChange} // Update search term on change
+            />
+          </InputGroup>
+        </div>
+        <TableContainer className="overflow-x-auto">
+          <Table variant="striped" colorScheme="gray" border={1}>
+            {homePageDatas.journeyDatas.length > 0 && (
+              <Thead className="bg-gray-400 h-[60px]">
+                <Tr>
+                  <Th>NO.</Th>
+                  {Object.keys(homePageDatas.journeyDatas[0]).map(
+                    (key, index) => (
+                      <Th key={index}>{key}</Th>
+                    )
+                  )}
+                  <Th>Action</Th>
+                </Tr>
+              </Thead>
+            )}
+            <Tbody>
+              {homePageDatas.journeyDatas.map((item, index) => (
+                <Tr key={index}>
+                  <Td>{index + 1}</Td>
+
+                  {Object.keys(item).map((key) => (
+                    <Td key={key}>
+                      {key === "image" ? (
+                        <div
+                          className="bg-gray-500 shadow-sm shadow-black rounded-lg h-[70px] w-[100px] bg-cover bg-center"
+                          style={{ backgroundImage: `url(${item[key]})` }}
+                        ></div>
+                      ) : key === "description_1" || key === "description_2" ? (
+                        <div className="w-[300px] overflow-hidden text-ellipsis">
+                          {item[key]}
+                        </div>
+                      ) : (
+                        item[key]
+                      )}
+                    </Td>
+                  ))}
+                  {/* actions button */}
+                  <Td>
+                    <HStack spacing="4">
+                      <Link href={`/cms/journey/detail/${item._id}`}>
+                        <IconButton
+                          icon={<InfoOutlineIcon />}
+                          colorScheme="blue"
+                          size="sm"
+                          aria-label="Detail"
+                        />
+                      </Link>
+                      <Link href={`/cms/journey/update/${item._id}`}>
+                        <IconButton
+                          icon={<EditIcon />}
+                          colorScheme="yellow"
+                          size="sm"
+                          aria-label="Update"
+                        />
+                      </Link>
+                      <IconButton
+                        icon={<DeleteIcon />}
+                        colorScheme="red"
+                        size="sm"
+                        aria-label="Delete"
+                        onClick={() => {
+                          changeDeletedId(item._id); // Set deletedItemId
+                          onOpen(); // Open the confirmation dialog
+                        }}
+                      />
+                    </HStack>
+                  </Td>
+                </Tr>
+              ))}
+            </Tbody>
+          </Table>
+        </TableContainer>
+        <div className="flex items-center justify-center align-middle mt-10">
+          <Pagination
+            count={homePageDatas.totalPage}
+            page={currentPage} // Controlled pagination
+            onChange={handlePageChange} // Update page on change
+            color="primary"
+            size="large"
+            shape="rounded"
+            className="bg-gray-400 rounded-lg"
+          />
+        </div>
+      </div>
+
+      {/* AlertDialog for Delete Confirmation */}
+      <AlertDialog
+        isOpen={isOpen}
+        leastDestructiveRef={cancelRef}
+        onClose={onClose}
+      >
+        <AlertDialogOverlay>
+          <AlertDialogContent>
+            <AlertDialogHeader fontSize="lg" fontWeight="bold">
+              Delete Confirmation
+            </AlertDialogHeader>
+
+            <AlertDialogBody>
+              Are you sure you want to delete this item? You can't undo this
+              action.
+            </AlertDialogBody>
+
+            <AlertDialogFooter>
+              <Button
+                ref={cancelRef}
+                onClick={onClose}
+                isDisabled={loadingDelete}
+              >
+                Cancel
+              </Button>
+              <Button
+                colorScheme="red"
+                onClick={handleDelete}
+                ml={3}
+                isLoading={loadingDelete} // Show loading state
+              >
+                Delete
+              </Button>
+            </AlertDialogFooter>
+          </AlertDialogContent>
+        </AlertDialogOverlay>
+      </AlertDialog>
+    </div>
+  );
+}
